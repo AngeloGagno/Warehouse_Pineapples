a231b4d2a3a95ad05cca0e1a85a8124230aadab6
angelogagno <angelogagno@gmail.com>
2025-07-23 10:21:07 -0300

    feat:Tabela payment checker para ingestao pelo sistema de baixa

---
+++ Pineapples/models/marts/finance/payment_checker.sql
@@ -0,0 +1,8 @@
+with source as (
+    select * from {{ref('int_confirmed_bookings')}}
+),
+filtered_portal_reference as (
+select booking_id, (gross_payment - portal_payment) as payment ,split_part(portal_reference,'-',2)  as portal_reference,  to_char(checkin_date, 'DD-MM-YYYY') AS checkin_date
+from source where sale_channel in ('Booking.com', 'Airbnb') and checkin_date between CURRENT_DATE - INTERVAL '365 days' and CURRENT_DATE )
+
+select * from filtered_portal_reference
